a823db016d888ca83037967a7a4a9897e0f06a55
quac88 <mac2@thrasher.com>
2025-03-01 23:19:11 -0600

    feat: start a file that finds the ticker names for macrotrend urls by checking yahoo finance for the names.
    Co-authored-by: Jordan Joelson <jordanjoelson@users.noreply.github.com>

---
+++ scrape/findTickerNames.js
@@ -0,0 +1,9 @@
+// List of tickers to process.
+const tickers = ["SPG"];
+
+// Define a constant for the User-Agent string.
+const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' +
+	'AppleWebKit/537.36 (KHTML, like Gecko) ' +
+	'Chrome/51.0.2704.103 Safari/537.36';
+
+
