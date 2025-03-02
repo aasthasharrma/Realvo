a443b1666d27610d19ee727122eaea5209d67542
quac88 <mac2@thrasher.com>
2025-03-01 23:39:39 -0600

    feat: add helper function that standardizes the yahoofinance slug ticker name to a format better for macrotrends

--- scrape/findTickerNames.js
+++ scrape/findTickerNames.js
@@ -6,4 +6,17 @@ const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' +
 	'AppleWebKit/537.36 (KHTML, like Gecko) ' +
 	'Chrome/51.0.2704.103 Safari/537.36';
 
-
+/**
+ * Converts a company name into a URL-friendly slug.
+ * Example: "Simon Property Group" -> "simon-property-group"
+ *
+ * @param {string} companySlug
+ * @returns {string} 
+ */
+function companySlugToName(companySlug) { // why did i even write this function?
+	return companySlug
+		.toLowerCase() // Convert to lowercase.
+		.replace(/[^\w\s]/g, '') // Remove non-alphanumeric characters.
+		.trim() // Remove leading/trailing spaces.
+		.replace(/\s+/g, '-'); // Replace spaces with hyphens.
+}
